40bffb5b31c0b83afd947717339c1c7f910ce348
nthhamster34 <2021154019@ub.edu.bz>
2023-05-19 21:44:02 -0600

    Second commit

--- triangle.go
+++ triangle.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-  "fmt"
-  "math"
+	"fmt"
+	"math"
 )
 
-
 type Triangle struct {
-  base   float64
-  height float64
+	base   float64
+	height float64
 }
 
 func (t Triangle) area() float64 {
-    return (t.height * t.base)/2
+	return (t.base * t.height) / 2
 }
 
-func (t Triangle) perimiter() float64 {
-    h := math.Sqrt(math.Pow(t.height, 2) + math.Pow(t.base, 2))
-    return h + t.height + t.base
+func (t Triangle) perimeter() float64 {
+	side := math.Sqrt(math.Pow(t.base/2, 2) + math.Pow(t.height, 2))
+	return 2*side + t.base
 }
 
 func main() {
-  triangle := Triangle {
-    base: 10, height: 12,
-  }
-  fmt.Println(triangle.area())
-  fmt.Println(triangle.perimiter())
+	triangle := Triangle{
+		base:   16, // Updated base value
+		height: 10, // Updated height value
+	}
+	fmt.Println(triangle.area())
+	fmt.Println(triangle.perimeter())
 }
--- triangle_test.go
+++ triangle_test.go
@@ -2,23 +2,23 @@ package main
 
 import "testing"
 
-
 func TestTriangle(t *testing.T) {
-  triangle := Triangle {
-    base: 10, height: 12,
-  }
+	triangle := Triangle{
+		base:   16,
+		height: 10,
+	}
 
-  expected_a := 60.0
-  got_a := triangle.area()
+	expected_a := 80.0 // Updated expected area
+	got_a := triangle.area()
 
-  expected_p := 37.62049935181331
-  got_p := triangle.perimiter()
+	expected_p := 41.612496949731394 // Updated expected perimeter
+	got_p := triangle.perimeter()
 
-  if got_a != expected_a {
-    t.Errorf("Expected %v got %v", expected_a, got_a)
-  }
+	if got_a != expected_a {
+		t.Errorf("Expected %v got %v", expected_a, got_a)
+	}
 
-  if got_p != expected_p {
-    t.Errorf("Expected %v got %v", expected_p, expected_p)
-  }
+	if got_p != expected_p {
+		t.Errorf("Expected %v got %v", expected_p, got_p)
+	}
 }
